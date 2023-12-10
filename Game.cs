9d620c2ff8e591e6345f04880f4b0bd9be4308ed
Hafthor <git@hafthor.net>
2023-12-10 02:10:09 -0500

    multijump

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 public class Game {
+    // TODO: Multiple jumps
     public bool isWhiteTurn = true;
     private char[,] board = new char[8, 8]; // 8x8 board with x(black) and o(white) - capital letters are kings
 
@@ -27,63 +28,75 @@ public class Game {
         return new string(chars);
     }
 
-    public char Move(int fromRow, int fromCol, int toRow, int toCol) {
-        if (!MoveLegal(fromRow, fromCol, toRow, toCol, isWhiteTurn)) throw new Exception("Illegal move");
-        var capture = MoveInternal(fromRow, fromCol, toRow, toCol);
-        if (capture == '.') isWhiteTurn = !isWhiteTurn;
-        return capture;
+    public List<char> Move(List<(int row, int col)> moves) {
+        if (!MoveLegal(moves, isWhiteTurn)) throw new Exception("Illegal move");
+        var captures = MoveInternal(moves);
+        isWhiteTurn = !isWhiteTurn;
+        return captures;
     }
 
     public static bool IsWhite(char piece) => piece is 'o' or 'O';
     public static bool IsKing(char piece) => piece is 'O' or 'X';
 
-    public static string MoveString(int fromRow, int fromCol, int toRow, int toCol) =>
-        "" + RC(fromRow, fromCol) + "-" + RC(toRow, toCol);
+    public static string MoveString(List<(int row, int col)> moves) =>
+        string.Join("-", moves.Select(m => RC(m.row, m.col)));
 
-    public static (int fromRow, int fromCol, int toRow, int toCol) ParseMove(string cmd) {
-        var parts = cmd.Split("-");
-        (int row, int col) from = RC(parts[0]), to = RC(parts[1]);
-        return (from.row, from.col, to.row, to.col);
-    }
+    public static List<(int row, int col)> ParseMove(string cmd) => 
+        cmd.Split("-").Select(s => RC(s)).ToList();
 
-    public bool MoveLegal(int fromRow, int fromCol, int toRow, int toCol, bool isWhite) {
-        if (toRow is < 0 or >= 8 || toCol is < 0 or >= 8 || fromRow == toRow || fromCol == toCol) return false;
-        var piece = board[fromRow, fromCol];
-        if (piece == '.') return false;
-        if (board[toRow, toCol] != '.') return false;
-        if (IsWhite(piece) != isWhite) return false;
-        int rowDiff = toRow - fromRow;
-        if (rowDiff == 0) return false;
-        if (rowDiff > 0 != isWhite && !IsKing(piece)) return false;
-        int colDiff = Math.Abs(fromCol - toCol);
-        if (Math.Abs(rowDiff) != colDiff || colDiff > 2) return false;
-        if (colDiff == 1) return true;
-        var capture = board[(fromRow + toRow) / 2, (fromCol + toCol) / 2];
-        return capture != '.' && IsWhite(capture) != isWhite;
-    }
-
-    private char MoveInternal(int fromRow, int fromCol, int toRow, int toCol) {
-        board[toRow, toCol] = board[fromRow, fromCol];
-        board[fromRow, fromCol] = '.';
-        if (Math.Abs(fromRow - toRow) == 2) {
-            var capture = board[(fromRow + toRow) / 2, (fromCol + toCol) / 2];
-            board[(fromRow + toRow) / 2, (fromCol + toCol) / 2] = '.';
-            return capture;
+    public bool MoveLegal(List<(int row, int col)> moves, bool isWhite) {
+        var from = moves[0];
+        var piece = board[from.row, from.col];
+        if (piece == '.' || IsWhite(piece) != isWhite) return false;
+        bool mustCapture = moves.Count > 2;
+        var prev = from;
+        HashSet<(int row, int col)> visited = new();
+        visited.Add(from);
+        for (int i = 1; i < moves.Count; prev=moves[i++]) {
+            var to = moves[i];
+            if (to.row is < 0 or >= 8 || to.col is < 0 or >= 8) return false;
+            if (!visited.Add(to)) return false;
+            if (board[to.row, to.col] != '.') return false;
+            int rowDiff = to.row - prev.row;
+            if (rowDiff == 0) return false;
+            if (rowDiff > 0 != isWhite && !IsKing(piece)) return false;
+            int colDiff = Math.Abs(prev.col - to.col);
+            if (Math.Abs(rowDiff) != colDiff || colDiff > 2) return false;
+            if (colDiff == 1) return !mustCapture;
+            var capture = board[(prev.row + to.row) / 2, (prev.col + to.col) / 2];
+            if (capture == '.' || IsWhite(capture) == isWhite) return false;
         }
-        return '.';
+        return true;
     }
 
-    public List<(int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score)> LegalMoves() {
+    private List<char> MoveInternal(List<(int row, int col)> moves) {
+        var from = moves[0];
+        var prev = from;
+        List<char> captures = new(moves.Count - 2);
+        for (int i = 1; i < moves.Count; prev = moves[i++]) {
+            var to = moves[i];
+            var piece = board[to.row, to.col] = board[prev.row, prev.col];
+            if (to.row is 0 or 7) board[to.row, to.col] = char.ToUpper(piece); // promote
+            if (Math.Abs(prev.row - to.row) == 2) {
+                var capture = board[(prev.row + to.row) / 2, (prev.col + to.col) / 2];
+                board[(prev.row + to.row) / 2, (prev.col + to.col) / 2] = '.';
+                captures.Add(capture);
+            }
+            board[prev.row, prev.col] = '.';
+        }
+        return captures;
+    }
+
+    public List<(List<(int row, int col)> move, char piece, List<char> captures, int score)> LegalMoves() {
         return LegalMoves(isWhiteTurn);
     }
 
-    private List<(int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score)> LegalMoves(bool isWhite) {
-        List<(int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score)> moves = new();
+    private List<(List<(int row, int col)> move, char piece, List<char> captures, int score)> LegalMoves(bool isWhite) {
+        List<(List<(int row, int col)> move, char piece, List<char> captures, int score)> moves = new();
         for (int r = 0; r < 8; r++)
             for (int c = 0; c < 8; c++)
                 if (board[r, c] != '.' && IsWhite(board[r, c]) == isWhite)
-                    foreach (var m in LegalMoves(r, c))
-                        moves.Add((r, c, m.toRow, m.toCol, board[r, c], m.capture, 0));
+                    moves.AddRange(LegalMoves((r, c)));
         return moves;
     }
 
@@ -92,27 +105,54 @@ public class Game {
         WhiteDirections = { (-1, 1), (1, 1) },
         BlackDirections = { (1, -1), (-1, -1) };
 
-    private List<(int toRow, int toCol, char capture)> LegalMoves(int fromRow, int fromCol) {
-        List<(int toRow, int toCol, char capture)> moves = new();
-        var piece = board[fromRow, fromCol];
+    private List<(List<(int row, int col)> move, char piece, List<char> captures, int score)> LegalMoves((int row, int col) from) {
+        List<(List<(int row, int col)> move, char piece, List<char> captures, int score)> moves = new();
+        var piece = board[from.row, from.col];
         bool isWhite = IsWhite(piece), isKing = IsKing(piece);
+        List<(int row, int col)> move = new() { (from.row, from.col) };
         foreach (var dir in isKing ? KingDirections : isWhite ? WhiteDirections : BlackDirections) {
-            if (MoveLegal(fromRow, fromCol, fromRow + dir.dy, fromCol + dir.dx, isWhite))
-                moves.Add((fromRow + dir.dy, fromCol + dir.dx, '.'));
-            if (MoveLegal(fromRow, fromCol, fromRow + dir.dy * 2, fromCol + dir.dx * 2, isWhite))
-                moves.Add((fromRow + dir.dy * 2, fromCol + dir.dx * 2, board[fromRow + dir.dy, fromCol + dir.dx]));
+            move.Add((from.row + dir.dy, from.col + dir.dx));
+            if (MoveLegal(move, isWhite))
+                moves.Add((move.ToList(), piece, new List<char>(), from.row + dir.dy is 0 or 7 ? 2 : 0));
+            move.RemoveAt(1);
+
+            move.Add((from.row + dir.dy * 2, from.col + dir.dx * 2));
+            if (MoveLegal(move, isWhite)) {
+                var capture = board[from.row + dir.dy, from.col + dir.dx];
+                moves.Add((move.ToList(), piece, new List<char>() { capture }, Value(capture, from.row + dir.dy) + from.row + dir.dy * 2 is 0 or 7 ? 2 : 0));
+                MultiJump(from.row + dir.dy * 2, from.col + dir.dx * 2, dir.dy);
+            }
+            move.RemoveAt(1);
         }
         return moves;
+
+        void MultiJump(int fromRow, int fromCol, int dy) {
+            move.Add((fromRow + dy * 2, fromCol - 2));
+            if (MoveLegal(move, isWhite)) {
+                var capture = board[fromRow + dy, fromCol - 1];
+                moves.Add((move.ToList(), piece, new List<char>() { capture }, Value(capture, fromRow + dy) + fromRow + dy * 2 is 0 or 7 ? 2 : 0));
+                MultiJump(fromRow + dy * 2, fromCol - 2, dy);
+            }
+            move.RemoveAt(move.Count - 1);
+
+            move.Add((fromRow + dy * 2, fromCol + 2));
+            if (MoveLegal(move, isWhite)) {
+                var capture = board[fromRow + dy, fromCol + 1];
+                moves.Add((move.ToList(), piece, new List<char>() { capture }, Value(capture, fromRow + dy) + fromRow + dy * 2 is 0 or 7 ? 2 : 0));
+                MultiJump(fromRow + dy * 2, fromCol + 2, dy);
+            }
+            move.RemoveAt(move.Count - 1);
+        }
     }
 
-    public (int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score) BestMove(Random random, int depth) {
+    public (List<(int row, int col)> move, char piece, List<char> captures, int score) BestMove(Random random, int depth) {
         var saves = new Game[depth];
         for (int i = 0; i < depth; i++) saves[i] = new Game(this);
         return BestMoveInternal(isWhiteTurn, random, depth, saves);
     }
 
-    private (int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score) BestMoveInternal(bool isWhite, Random random, int depth, Game[] saves) {
-        (int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score) bestMove = default;
+    private (List<(int row, int col)> move, char piece, List<char> captures, int score) BestMoveInternal(bool isWhite, Random random, int depth, Game[] saves) {
+        (List<(int row, int col)> move, char piece, List<char> captures, int score) bestMove = default;
         var (bestScore, bestMovesCount, moves) = (int.MinValue, 0, LegalMoves(isWhite));
         if (depth == 0)
             foreach (var move in moves)
@@ -120,28 +160,25 @@ public class Game {
         else {
             saves[depth - 1].Restore(this);
             foreach (var move in moves) {
-                var capture = MoveInternal(move.fromRow, move.fromCol, move.toRow, move.toCol);
-                if (capture == '.')
-                    BestScore(move.score - BestMoveInternal(!isWhite, random, depth - 1, saves).score, move);
-                else
-                    BestScore(move.score + BestMoveInternal(isWhite, random, depth, saves).score, move);
+                var capture = MoveInternal(move.move);
+                BestScore(move.score - BestMoveInternal(!isWhite, random, depth - 1, saves).score, move);
                 Restore(saves[depth - 1]);
             }
         }
         return bestMove;
 
-        void BestScore(int score, (int fromRow, int fromCol, int toRow, int toCol, char piece, char capture, int score) move) {
+        void BestScore(int score, (List<(int row, int col)> move, char piece, List<char> captures, int score) move) {
             if (score > bestScore)
                 (bestScore, bestMove, bestMovesCount) = (score, move, 1);
             else if (score == bestScore && random.Next(++bestMovesCount) == 0)
                 bestMove = move;
         }
     }
-    
+
     private static int Value(char piece, int row) => piece switch {
         '.' => 0,
-        'x' => row == 1 ? 2 : 1, // about to be promoted=2
-        'o' => row == 6 ? 2 : 1, // about to be promoted=2
+        'x' => row == 0 ? 3 : row == 1 ? 2 : 1, // about to be promoted=2
+        'o' => row == 7 ? 3 : row == 6 ? 2 : 1, // about to be promoted=2
         'X' or 'O' => 3,
         _ => throw new Exception("unknown piece")
     };
--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@
                 Console.WriteLine(" No legal moves!");
                 break;
             }
-            Console.WriteLine(" Moves: " + string.Join(", ", moves.Select(m => Game.MoveString(m.fromRow, m.fromCol, m.toRow, m.toCol))));
+            Console.WriteLine(" Moves: " + string.Join(", ", moves.Select(m => Game.MoveString(m.move))));
             var cmd = autoMode ? "" : Console.ReadLine();
             if (cmd == "") cmd = game.isWhiteTurn ? lastWhiteCmd : lastBlackCmd;
             if (cmd == "auto")
@@ -21,16 +21,16 @@
             else if (cmd.StartsWith("best")) {
                 _ = game.isWhiteTurn ? lastWhiteCmd = cmd : lastBlackCmd = cmd;
                 var move = game.BestMove(random, int.Parse(cmd.Substring(4)));
-                Console.WriteLine("Move: " + Game.MoveString(move.fromRow, move.fromCol, move.toRow, move.toCol));
-                game.Move(move.fromRow, move.fromCol, move.toRow, move.toCol);
+                Console.WriteLine("Move: " + Game.MoveString(move.move));
+                game.Move(move.move);
             } else if (cmd.Contains("-")) {
                 var move = Game.ParseMove(cmd);
-                if (!game.MoveLegal(move.fromRow, move.fromCol, move.toRow, move.toCol, game.isWhiteTurn)) {
+                if (!game.MoveLegal(move, game.isWhiteTurn)) {
                     Console.WriteLine("Invalid move");
                     continue;
                 }
                 Console.WriteLine("Move: " + cmd);
-                game.Move(move.fromRow, move.fromCol, move.toRow, move.toCol);
+                game.Move(move);
             }
         }
     }
